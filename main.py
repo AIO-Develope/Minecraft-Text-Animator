b6568fc7fde057842fbc4b6c1a0c30ac2d6c7dc1
AIO <69240351+AIO-Develope@users.noreply.github.com>
2022-03-26 19:46:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+#prototype version
+
+u_text = input("Text: ")
+format = input("format: ")
+
+def split(u_text):
+    return [char for char in u_text]
+     
+c_text = len(u_text)
+s_text = split(u_text)
+
+f = open('output.txt', 'r+')
+f.truncate(0)
+
+if format == "Raw":
+
+    for i in range(c_text + 1):
+        s_text.insert(i, '&d')
+        s_text.insert(i + 2, '&r')
+        print("".join(s_text))
+        
+        f = open("output.txt", "a")
+        f.write("".join(s_text) + "\n")
+        f.close()
+
+        try:
+            
+            del s_text[i + 2]
+            del s_text[i]
+        except:
+            pass
+
+elif format == "Tab":
+    for i in range(c_text + 1):
+        s_text.insert(i, '&d')
+        s_text.insert(i + 2, '&r')
+        print("  - '" + "".join(s_text) + "'")
+        
+        f = open("output.txt", "a")
+        f.write("  - '" + "".join(s_text) + "'\n")
+        f.close()
+    
+        
+        try:
+            
+            del s_text[i + 2]
+            del s_text[i]
+        except:
+            pass
+
+elif format == "Scoreboard":
+    for i in range(c_text + 1):
+        s_text.insert(i, '&d')
+        s_text.insert(i + 2, '&r')
+        print("        - '" + "".join(s_text) + "'")
+        
+        f = open("output.txt", "a")
+        f.write("        - '" + "".join(s_text) + "'\n")
+        f.close()
+    
+        
+        try:
+            
+            del s_text[i + 2]
+            del s_text[i]
+        except:
+            pass
+
+
+
